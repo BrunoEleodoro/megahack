c09965214a15fa2abe828f76f5be194c13b8b54d
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-02-18 11:49:50 -0300

    Updates

---
+++ precache-manifest.5580b7a1fa54b39b2c04788ff206e150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae4c4fd5d94df51d9e50b82f15c743b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0248490d646de6424667",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f9f068eb73cf127a49d3",
+    "url": "/static/js/2.38580200.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.38580200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0248490d646de6424667",
+    "url": "/static/js/main.c54924b6.chunk.js"
+  },
+  {
+    "revision": "85e6a98b86ef3993b97a",
+    "url": "/static/js/runtime-main.5de74a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
